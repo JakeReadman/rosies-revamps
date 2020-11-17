2a2252d8b42f38a6ee56c326e0591769a04f1d70
Jake Readman <jakereadman@gmail.com>
2020-11-17 15:23:49 +0000

    update index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.render('home.html');
+  res.render('/public/home');
 });
 
 app.use(express.static(__dirname + '/public'));
