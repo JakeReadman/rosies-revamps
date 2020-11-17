5e8ed14207eff38c0d5fc3fe4407a3e4d805b171
Jake Readman <jakereadman@gmail.com>
2020-11-17 16:04:55 +0000

    try to deploy

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const flash = require('connect-flash');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(__dirname + '/public'));
+app.use(flash());
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.use(express.static(__dirname + '/public'));
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+  res.status(statusCode).render('error', { err });
+});
 
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
